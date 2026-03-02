9b416a816a9cc70d3c28b42ed0b24e3726578ace
Eli Thomas <ecthomas05@gmail.com>
2026-03-02 23:06:19 +0000

    added min/avg/max RTT time and packet loss

--- UDPPingerClient.py
+++ UDPPingerClient.py
@@ -4,18 +4,36 @@ from time import clock_gettime_ns, CLOCK_MONOTONIC
 clientSocket = socket(AF_INET, SOCK_DGRAM);
 clientSocket.settimeout(1)
 
+server_ip = '100.96.11.84'
 
-for i in range(10):
+minRTT = float('inf')
+maxRTT = float('-inf')
+avgRTT = 0
+numPackets = 10
+numLost = 0
+
+
+for i in range(numPackets):
     sendTime = clock_gettime_ns(CLOCK_MONOTONIC)
     message = f"Ping {i + 1} {sendTime}"
-    clientSocket.sendto(message.encode(), ("100.114.252.36", 12000))
+    clientSocket.sendto(message.encode(), (server_ip, 12000))
     try:
         response = clientSocket.recv(1024).decode()
         responseTime = clock_gettime_ns(CLOCK_MONOTONIC)
-        RTT = (responseTime - sendTime) / 1e9
-        print(f"Response: {response}\nRTT: {RTT:.6f} sec")
+        RTT = (responseTime - sendTime) / 1e6
+        if RTT < minRTT or RTT > maxRTT:
+            minRTT = min(minRTT, RTT)
+            maxRTT = max(maxRTT, RTT)
+        avgRTT += RTT
+        print(f"Response: {response}\nRTT: {RTT:.6f} ms")
     except TimeoutError:
+        numLost += 1
         print("Request timed out")
     print("\n")
 
-print("---------------------------")
+avgRTT /= numPackets
+
+print(f"-------------Ping Stats for {server_ip}-------------")
+print(f"Packet Loss: {numLost / numPackets * 100}%")
+print(f"RTT min/avg/max : {minRTT:.3f}/{avgRTT:.3f}/{maxRTT:.3f} ms")
+
